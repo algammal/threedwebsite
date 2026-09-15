504b4f318466c043c340837554fef3d424add9c0
DESKTOP-4HT4TEV\ahmed <ahmed_nader88@icloud.com>
2026-09-15 16:08:28 -0700

    fixed page import

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,4 +1,4 @@
-import MainLayout from "../../layouts/MainLayout/MainLayout";
+import MainLayout from "../../layouts/mainLayout/MainLayout";
 import HeroScene from "../../scenes/hero/HeroScene"
 
 function Home() {
